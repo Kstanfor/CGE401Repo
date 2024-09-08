131383f90c332cd3da1930ffbd79b6c6ca973f97
Kstanfor <158608252+Kstanfor@users.noreply.github.com>
2024-09-08 17:43:55 -0500

    BeforeScoreManager

---
+++ 401Challenge1/Assets/Challenge 1/Scripts/OutOfBounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+//attach to player
+
+public class OutOfBounds : MonoBehaviour
+{
+    public Text textbox;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -51) 
+        {
+            textbox.text = "You Lose!";
+        }
+
+        if (transform.position.y > 80)
+        {
+            textbox.text = "You Lose!";
+        }
+    }
+}
