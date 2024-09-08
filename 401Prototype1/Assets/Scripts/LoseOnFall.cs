07a18b9b1f7218036c47f093bc436a1226c6b13f
Kstanfor <158608252+Kstanfor@users.noreply.github.com>
2024-09-08 15:58:26 -0500

    Prototype1RestartDone

---
+++ 401Prototype1/Assets/Scripts/LoseOnFall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+
+//attach to player
+public class LoseOnFall : MonoBehaviour
+{
+    
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -1) 
+        {
+            ScoreManager.gameOver = true;
+        }
+    }
+}
---
+++ 401Prototype1/Assets/Scripts/PlayerEnterTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+
+public class PlayerEnterTrigger : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TriggerZone"))
+        {
+            ScoreManager.score++;
+        }
+    }
+}
