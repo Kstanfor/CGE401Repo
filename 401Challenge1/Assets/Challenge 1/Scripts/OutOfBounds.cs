f5abf4fc06b53b226a2d58091cbb6beb6ce6a314
Kstanfor <158608252+Kstanfor@users.noreply.github.com>
2024-09-08 18:11:03 -0500

    RestartWorks

--- 401Challenge1/Assets/Challenge 1/Scripts/OutOfBounds.cs
+++ 401Challenge1/Assets/Challenge 1/Scripts/OutOfBounds.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Security.Cryptography;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 //attach to player
 
 public class OutOfBounds : MonoBehaviour
 {
-    public Text textbox;
+    
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.y < -51) 
         {
-            textbox.text = "You Lose!";
+            ScoreManager.gameOver = true;
         }
 
         if (transform.position.y > 80)
         {
-            textbox.text = "You Lose!";
+            ScoreManager.gameOver = true;
         }
     }
 }
---
+++ 401Challenge1/Assets/Challenge 1/Scripts/ScoreManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+
+    public Text textbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+
+        if (score>=5)
+        {
+            won = true;
+            gameOver = true;
+        }
+
+        if (gameOver) 
+        {
+            if (won)
+            {
+                textbox.text = "You Win!\nPress R to try Again";
+
+            }
+            else 
+            {
+                textbox.text = "You Lose!\nPress R to try Again";
+
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
