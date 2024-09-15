d9c602a4afe3ab96a34f2b80e8806ce455bc6aa7
Kstanfor <158608252+Kstanfor@users.noreply.github.com>
2024-09-15 15:58:46 -0500

    animals spawn with random delay

--- 401Prototype 2/Assets/Scripts/SpawnManager.cs
+++ 401Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,15 +5,45 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
    public GameObject[] prefabsToSpawn;
+    private float leftBound = 17;
+    private float rightBound = -17;
+    private float spawnPosZ = 20;
 
+    void Start()
+    {
+        //InvokeRepeating("SpawnRandomPrefab", 2, 1.5f);
+        StartCoroutine(SpawnRandomPrefabWithCoroutine());
+
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
-
-            Instantiate(prefabsToSpawn[prefabIndex], new Vector3(0, 0, 20), prefabsToSpawn[prefabIndex].transform.rotation);
+           //SpawnRandomPrefab();
         }
+    }
+
+    IEnumerator SpawnRandomPrefabWithCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (true)
+        {
+            SpawnRandomPrefab();
+
+            float randomDelay = Random.Range(0.8f, 2.0f);
+
+            yield return new WaitForSeconds(randomDelay);
+        }
+        }
+
+    void SpawnRandomPrefab()
+    {
+        int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
+
+        Vector3 spawnPos = new Vector3(Random.Range(leftBound, rightBound), 0, spawnPosZ);
+
+        Instantiate(prefabsToSpawn[prefabIndex], spawnPos, prefabsToSpawn[prefabIndex].transform.rotation);
     }
 }
