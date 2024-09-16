8d1f5e6091e0d5cd9435928cbc7c9b0ade98f547
Kstanfor <158608252+Kstanfor@users.noreply.github.com>
2024-09-16 16:00:17 -0500

    starting health system

---
+++ 401Challangev2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        StartCoroutine(SpawnAtRandTime());
+
+    }
+
+    IEnumerator SpawnAtRandTime()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (true)
+        {
+            SpawnRandomBall();
+
+            float randomDelay = Random.Range(3f, 5f);
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        int prefabIndex = Random.Range(0, ballPrefabs.Length);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[prefabIndex], spawnPos, ballPrefabs[prefabIndex].transform.rotation);
+    }
+
+}
