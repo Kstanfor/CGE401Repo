2d9972e481db716e1dddd99327f0e07988e29287
Kstanfor <158608252+Kstanfor@users.noreply.github.com>
2024-11-18 18:44:02 -0600

    MoveWithCamera

--- Prototype4Stanford/Assets/Scripts/PlayerController.cs
+++ Prototype4Stanford/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody playerRb;
     public float speed = 5.0f;
+    private float forwardInput;
+
+    private GameObject focalPoint;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.FindGameObjectWithTag("FocalPoint");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        forwardInput = Input.GetAxis("Vertical");
+        
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
